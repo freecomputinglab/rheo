4a5aedd6ddbd3bf9177613308b9667ecc4c9b592
Lachlan Kermode <lachie@ohrg.org>
2025-12-25 19:01:09 +0100

    Tests multiple errors formatting

--- tests/harness.rs
+++ tests/harness.rs
@@ -34,6 +34,7 @@ use std::path::PathBuf;
 #[test_case("tests/cases/error_formatting/unknown_function.typ")]
 #[test_case("tests/cases/error_formatting/invalid_method.typ")]
 #[test_case("tests/cases/error_formatting/invalid_field.typ")]
+#[test_case("tests/cases/error_formatting/multiple_errors.typ")]
 fn run_test_case(name: &str) {
     let test_case = TestCase::new(name);
     let update_mode = env::var("UPDATE_REFERENCES").is_ok();
