6f1fd02aae54fd5f6e2a8a0dd718f9f107491da8
Lachlan Kermode <lachie@ohrg.org>
2026-03-25 11:35:58 +0100

    Adds all_features example to test cases

--- crates/tests/tests/harness.rs
+++ crates/tests/tests/harness.rs
@@ -11,6 +11,7 @@ use std::path::PathBuf;
 
 #[test_case("../../examples/blog_site")]
 #[test_case("../../examples/blog_post")]
+#[test_case("../../examples/all_features")]
 #[test_case("../../examples/cover-letter.typ")]
 #[test_case("../../examples/blog_site/content/index.typ")]
 #[test_case("../../examples/blog_site/content/severance-ep-1.typ")]
